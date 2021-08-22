b5cb1b32389dae3951616830b2527e82c2b2a009
Max Heydasch <max@staff-200-215.eduroam.unibe.ch>
2021-08-22 21:10:56 +0200

    list change

--- lib/main.dart
+++ lib/main.dart
@@ -15,17 +15,16 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'VocTrainer',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          primarySwatch: Colors.amber),
       home: MyHomePage(),
     );
   }
@@ -70,6 +69,10 @@ class MyHomePage extends StatelessWidget {
       ],
     } //map
   ];
+
+  var _questionList = {
+    'Language?': ['German', 'English']
+  };
 
   @override
   Widget build(BuildContext context) {
@@ -122,9 +125,7 @@ class MyHomePage extends StatelessWidget {
               ),
             ),
           ),
-          QuestionPrompt(questionText: {
-            'Language?': ['German', 'English']
-          }),
+          QuestionPrompt(questionText: _questionList),
         ]),
       ),
     );
